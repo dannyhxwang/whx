93a28e9cc48a5ccce4b2ea4ca6869fc5445c0f95
yanbit <yankunhadoop@gmail.com>
2015-12-15 15:56:54 +0800

    test

--- src/main/java/com/dsk/storm/service/RequestCount.java
+++ src/main/java/com/dsk/storm/service/RequestCount.java
@@ -7,18 +7,20 @@ import backtype.storm.generated.AuthorizationException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.tuple.Fields;
-import com.dsk.storm.function.PrintFunction;
 import com.dsk.storm.function.RequestCountETL;
+import com.dsk.storm.state.RedisState;
 import com.dsk.utils.Constants;
 import storm.kafka.BrokerHosts;
 import storm.kafka.StringScheme;
 import storm.kafka.ZkHosts;
 import storm.kafka.trident.TransactionalTridentKafkaSpout;
 import storm.kafka.trident.TridentKafkaConfig;
-import storm.trident.Stream;
+import storm.trident.TridentState;
 import storm.trident.TridentTopology;
 import storm.trident.operation.builtin.Count;
+import storm.trident.state.StateFactory;
 
+import java.net.InetSocketAddress;
 import java.util.UUID;
 
 /**
@@ -32,18 +34,20 @@ public class RequestCount {
         BrokerHosts hosts = new ZkHosts(Constants.ZOOKEEPER_LIST);
         TridentKafkaConfig tridentKafkaConfig =
                 new TridentKafkaConfig(hosts, Constants.TOPIC_REQUEST_COUNT, UUID.randomUUID().toString());
-        tridentKafkaConfig.startOffsetTime= kafka.api.OffsetRequest.EarliestTime();
         tridentKafkaConfig.ignoreZkOffsets=true;
         tridentKafkaConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
         TransactionalTridentKafkaSpout tridentKafkaSpout = new TransactionalTridentKafkaSpout(tridentKafkaConfig);
 
+        // state
+        StateFactory state = RedisState.transactional(new InetSocketAddress("namenode", 6379), Constants.TOPIC_REQUEST_COUNT);
         // count request
         TridentTopology topology = new TridentTopology();
-        Stream test = topology.newStream(Constants.TOPIC_REQUEST_COUNT, tridentKafkaSpout)
+        TridentState test = topology.newStream(Constants.TOPIC_REQUEST_COUNT, tridentKafkaSpout)
                 .each(new Fields("str"), new RequestCountETL(), new Fields("word"))
                 .groupBy(new Fields("word"))
-                .aggregate(new Fields("word"),new Count(), new Fields("count"))
-                .each(new Fields("word","count"),new PrintFunction(),new Fields("print"));
+                .persistentAggregate(state,new Count(), new Fields("count"));
+
+                //.each(new Fields("word","count"),new PrintFunction(),new Fields("print"));
 
         Config conf = new Config();
         conf.setNumWorkers(1);
