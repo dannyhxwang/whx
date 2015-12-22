57266e360a5f6800cffe3a7a7f315f46ef5da31e
dannyhxwang <dannyhxwang@gmail.com>
2015-12-22 14:29:40 +0800

    upmatch

--- src/main/java/com/dsk/storm/bolts/UpMatchBolt.java
+++ src/main/java/com/dsk/storm/bolts/UpMatchBolt.java
@@ -33,45 +33,54 @@ public class UpMatchBolt extends BaseRichBolt {
     //XXXXX,stu_t,TAB_STARTUP_APP,aWRtYW4kaWRtYW4uZXhl,RGlzYWJsZQ==,6.7.69,us,isafe,1,20151216
     @Override
     public void execute(Tuple tuple) {
-        Object obj = tuple.getValue(0);
-        String line = obj.toString();
-//        String line = tuple.getString(0);
-        String items[] = line.split(",");
-        if(items.length == 10) {
-//            System.out.println(line);
-            String currentDate = items[9].trim();
-
-            //如果当前数据日期与上一条数据日期不同，则认为前一天的数据发送完毕
-//            if (!currentDate.equals(preDate)) {
-            String sourceComponent = tuple.getSourceComponent();
-            if(sourceComponent.equals(backtype.storm.Constants.SYSTEM_COMPONENT_ID)) {
-//            if (num > 20000) {
-                //store to hbase
-                if (dataMap.size() > 0) {
-                    System.out.println("----------------------------" + dataMap.size());
-                    new HbaseTask(dataMap).dowork();
-//                    num = 0;
-                }
-            } else {
-                String rowkey = StringOperator.encryptByMd5(items[0] + items[1] + items[2] + items[3] + items[4]);
-                UpMatcher upMatcher = dataMap.get(rowkey);
-                int count = 1;
-                if (StringUtils.isNotBlank(items[8])) {
-                    count = Integer.valueOf(items[8]);
-                }
-                if (upMatcher != null) {
-                    count += upMatcher.getCount();
-                    upMatcher.setCount(count);
-                } else {
-                    upMatcher = new UpMatcher(items[0], items[1], items[2], items[3], items[4], items[5],
-                            items[6], items[7], count);
-                }
-                dataMap.put(rowkey, upMatcher);
-//                num++;
+        String sourceComponent = tuple.getSourceComponent();
+        if (sourceComponent.equals(backtype.storm.Constants.SYSTEM_COMPONENT_ID)) {
+            if (dataMap.size() > 0) {
+                System.out.println("----------------------------" + dataMap.size());
+                new HbaseTask(dataMap).dowork();
             }
+        } else {
+            Object obj = tuple.getValue(0);
+            String line = obj.toString();
+//        String line = tuple.getString(0);
+            String items[] = line.split(",");
+            if(items.length == 10) {
+//            System.out.println(line);
+//                String currentDate = items[9].trim();
+
+                //如果当前数据日期与上一条数据日期不同，则认为前一天的数据发送完毕
+//            if (!currentDate.equals(preDate)) {
+
+                /*if (num > 20000) {
+                    //store to hbase
+                    if (dataMap.size() > 0) {
+                        System.out.println("----------------------------" + dataMap.size());
+                        new HbaseTask(dataMap).dowork();
+//                    num = 0;
+                    }
+                } else {*/
+                    String rowkey = StringOperator.encryptByMd5(items[0] + items[1] + items[2] + items[3] + items[4]);
+                    UpMatcher upMatcher = dataMap.get(rowkey);
+                    int count = 1;
+                    if (StringUtils.isNotBlank(items[8])) {
+                        count = Integer.valueOf(items[8]);
+                    }
+                    if (upMatcher != null) {
+                        count += upMatcher.getCount();
+                        upMatcher.setCount(count);
+                    } else {
+                        upMatcher = new UpMatcher(items[0], items[1], items[2], items[3], items[4], items[5],
+                                items[6], items[7], count);
+                    }
+                    dataMap.put(rowkey, upMatcher);
+//                num++;
+//                }
 //            preDate = currentDate;
 
+            }
+
         }
+
         this.collector.ack(tuple);
 
     }
