f01d557635eeaa3515438120a54913d70552da21
dannyhxwang <dannyhxwang@gmail.com>
2015-12-22 16:17:56 +0800

    upmatch

--- src/main/java/com/dsk/hbase/HbaseTask.java
+++ src/main/java/com/dsk/hbase/HbaseTask.java
@@ -71,7 +71,9 @@ public class HbaseTask {
             Table table = null;
             try {
                 table = conn.getTable(TableName.valueOf(Constants.UPMATCH_TABLE));
+                table.setWriteBufferSize(1024*1024*2);
                 table.put(puts);
+                System.out.println("===============puts size=================" + puts.size());
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
