5c8a789967a7366d58e6a4d5a61cd8fc7b4f8ed0
yanbit <yankunhadoop@gmail.com>
2015-12-25 11:28:58 +0800

    test hbase upmatch

--- src/main/java/com/dsk/storm/test/TestProducer.java
+++ src/main/java/com/dsk/storm/test/TestProducer.java
@@ -6,6 +6,7 @@ import kafka.producer.ProducerConfig;
 
 import java.util.Properties;
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * User: yanbit
@@ -15,7 +16,7 @@ import java.util.Random;
 public class TestProducer {
     public static void main(String[] args) {
         Random rnd = new Random();
-        int events = 10000;
+        int events = 10000000;
 
         Properties props = new Properties();
         props.put("metadata.broker.list", "namenode:9092,datanode1:9092,datanode4:9092");
@@ -29,11 +30,12 @@ public class TestProducer {
         // 产生并发送消息
         long start = System.currentTimeMillis();
         for (long i = 0; i < events; i++) {
-            String value = "3,2,3,4,5,6,7,8,5,10";
+            String value = UUID.randomUUID().toString() + ",2,3,4,5,6,7,8,5,10";
             //如果topic不存在，则会自动创建，默认replication-factor为1，partitions为0
             KeyedMessage<String, String> data = new KeyedMessage<String, String>(
-                    "test_count", null, value);
+                    "test_rk", null, value);
             producer.send(data);
+            System.out.println(value);
         }
         System.out.println("耗时:" + (System.currentTimeMillis() - start));
         // 关闭producer
